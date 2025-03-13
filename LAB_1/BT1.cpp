2b02169b0c5300aed42a8e0823467227f0092498
Trần Thanh Nhân <134724987+normalman159@users.noreply.github.com>
2025-03-13 20:42:14 +0700

    first init

---
+++ LAB_1/BT1.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct PhanSo{
+    int ts, ms;
+    PhanSo() : ts(0), ms(0) {}
+};
+
+void input(PhanSo &A){
+    do{
+        cout << "Hay nhap vao tu so: "; cin >> A.ts;
+        cout << "Hay nhap vao mau so: "; cin >> A.ms;
+        if (A.ms == 0) cout << "Mau so khong duoc bang 0, vui long nhap lai!\n"; 
+    }while(A.ms == 0);
+}
+
+int getUCLN(int a, int b){
+    return a == 0 ? b : getUCLN(b % a, a);
+}
+
+void rutGon(PhanSo &A){
+    int UCLN = getUCLN(A.ts, A.ms);
+    A.ts /= UCLN, A.ms /= UCLN;
+}
+
+void output(const PhanSo &A){
+    cout << "Phan so sau khi duoc rut gon: " << A.ts << "/" << A.ms;
+}
+
+int main(){
+    PhanSo A;
+    input(A);
+    rutGon(A);
+    output(A);
+
+    return 0;
+}
---
+++ LAB_1/BT2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct PhanSo{
+    int ts, ms;
+    PhanSo() : ts(0), ms(0) {}
+};
+
+void input(PhanSo &A, PhanSo &B){
+    do{
+        cout << "Hay nhap vao tu so thu nhat: "; cin >> A.ts;
+        cout << "Hay nhap vao mau so thu nhat: "; cin >> A.ms;
+        if (A.ms == 0) cout << "Mau so khong duoc bang 0, vui long nhap lai!\n"; 
+    }while(A.ms == 0);
+
+    do{
+        cout << "Hay nhap vao tu so thu hai: "; cin >> B.ts;
+        cout << "Hay nhap vao mau so thu hai: "; cin >> B.ms;
+        if (B.ms == 0) cout << "Mau so khong duoc bang 0, vui long nhap lai!\n"; 
+    }while(B.ms == 0);
+}
+
+void timPhanSoLonNhat(PhanSo &A, PhanSo &B, PhanSo &ANS){
+    if (A.ts * B.ms >= B.ts * A.ms){
+        ANS = A;
+    }
+    else{
+        ANS = B;
+    }
+}
+
+void output(const PhanSo &A){
+    cout << "Phan so lon nhat trong hai phan so la: " << A.ts << "/" << A.ms;
+}
+
+int main(){
+    PhanSo A,B, ANS;
+    input(A,B);
+    timPhanSoLonNhat(A,B,ANS);
+    output(ANS);
+
+    return 0;
+}
